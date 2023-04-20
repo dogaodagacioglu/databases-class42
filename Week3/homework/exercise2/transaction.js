e1ef1ae2ca22c2d69f7313285c27e56142944b12
Doğa ödağacıoğlu <agarwoodson@icloud.com>
2023-04-20 16:20:37 +0300

    imorove my code after review

--- Week3/homework/exercise2/transaction.js
+++ Week3/homework/exercise2/transaction.js
@@ -16,10 +16,13 @@ db.connect((err) => {
   console.log("mysql connected...");
 });
 
+db.query("START TRANSACTION");
+
 db.query(
   "UPDATE account SET balance = balance-1000 WHERE account_number = 101",
   (err) => {
     if (err) {
+      db.query("ROLLBACK");
       throw err;
     }
     console.log("1000 euros was sent from the account 101");
@@ -30,6 +33,7 @@ db.query(
   "UPDATE account SET balance = balance+1000 WHERE account_number = 102",
   (err) => {
     if (err) {
+      db.query("ROLLBACK");
       throw err;
     }
     console.log("1000 euros deposited to the account 102");
@@ -40,6 +44,7 @@ db.query(
   'INSERT INTO account_changes (account_number, amount, changed_date, remark) VALUES (101, -1000, "2023-05-10","Money transfer from account 101 to account 102")',
   (err) => {
     if (err) {
+      db.query("ROLLBACK");
       throw err;
     }
   }
@@ -48,6 +53,7 @@ db.query(
   'INSERT INTO account_changes (account_number, amount, changed_date, remark) VALUES (102, 1000, "2023-05-10","Money transfer from account 101 to account 102")',
   (err) => {
     if (err) {
+      db.query("ROLLBACK");
       throw err;
     }
   }
@@ -55,6 +61,7 @@ db.query(
 
 db.query("COMMIT", (err) => {
   if (err) {
+    db.query("ROLLBACK");
     throw err;
   }
   console.log("Transfer complete.");
